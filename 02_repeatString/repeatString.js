cdda0a75c338d10e72d7d3e60003e2998c118fcb
Kevin Martinez <mkevinez@gmail.com>
2023-07-28 16:51:40 -0700

    Completed exercises 01 and 02 with passing tests.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repStr = "";
+    if (num < 0) {
+        repStr += "ERROR";
+    } else {
+        for (i=1; i<=num; i++) {
+            repStr += string;
+        }
+    }
+    return repStr
 };
 
 // Do not edit below this line
