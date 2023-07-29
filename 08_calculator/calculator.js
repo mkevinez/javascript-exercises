f35568f2d1ded46a3f89583c0b30a809563b442b
Kevin Martinez <mkevinez@gmail.com>
2023-07-29 16:49:37 -0700

    completed exercise 08 and passed tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+  let total = 0;
+  for (let num of numArray) {
+    total += num;
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  let total = 1;
+  for (let num of numArray) {
+    total *= num;
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1;
+  if (num > 0) {
+    for (let i = num; i > 0; i--) {
+      total *= i;
+    }
+  }
+  return total;
 };
 
 // Do not edit below this line
