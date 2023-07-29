3a0bb002ded609de07d12c96b190eb64c9863cc9
Kevin Martinez <mkevinez@gmail.com>
2023-07-28 17:55:43 -0700

    completed exercise 05 and passed tests.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(firstNum, lastNum) {
+    let sum = 0;
+    let numPair = [firstNum, lastNum];
+    numPair.sort()
 
+    let filterNumPair = numPair.filter(num => Number.isInteger(num) && num > 0)
+
+    if (filterNumPair.length < 2) {
+        return "ERROR"
+    } else {
+        for (i=numPair[0]; i<=numPair[1]; i++) {
+            sum += i;
+        }
+        return sum
+    }
 };
 
 // Do not edit below this line
