c3ab0b52a64dcd6b51f3acf428479309bbd1ef45
Kevin Martinez <mkevinez@gmail.com>
2023-07-29 16:31:03 -0700

    completed exercise 07 and passed tests.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  convertedTemp = (temp - 32) * (5 / 9);
+  temp = Math.round(convertedTemp * 10) / 10;
+  return temp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  convertedTemp = temp * (9 / 5) + 32;
+  temp = Math.round(convertedTemp * 10) / 10;
+  return temp;
 };
 
 // Do not edit below this line
