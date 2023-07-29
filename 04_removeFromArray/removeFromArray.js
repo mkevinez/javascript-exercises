0e305be1459a33dab142f652804f0a7bb4e843a4
Kevin Martinez <mkevinez@gmail.com>
2023-07-28 17:39:00 -0700

    completed exercise 04 with tests passed.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    for (let arg of args) {
+        let i = array.indexOf(arg);
+        while (i !== -1) {
+            array.splice(i,1);
+            i = array.indexOf(arg);
+        }
+    }
+    return array
 };
 
 // Do not edit below this line
