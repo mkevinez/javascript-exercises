2f77ccfa7ffbf9acffc5bb7acd6a6cf5c8051646
Kevin Martinez <mkevinez@gmail.com>
2023-07-30 14:50:26 -0700

    completed exercise 10 and passed tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    let fibArray = [1,1];
+    if (index < 0) return "OOPS";
+    if (index >= 3) {
+        for (let i = 2; i <  index; i++) {
+            fibArray.push(fibArray[i-1] + fibArray[i-2]);
+        }
+    }
+    return fibArray[index-1];
 };
 
 // Do not edit below this line
