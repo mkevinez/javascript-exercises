afa63675815449d4b7797d668a6593a2fb608e53
Kevin Martinez <mkevinez@gmail.com>
2023-07-30 15:40:14 -0700

    completed exercise 12 and passed tests.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArray) {
+    
+    oldestArray = peopleArray.reduce(returnOldest);
 
+    function returnOldest(older, person) {
+        if (getAge(older) > getAge(person)) {
+            return older;
+        } else {
+            return person;
+        }
+    }
+
+    function getAge(personObj) {
+        let today = new Date();
+        if (personObj.yearOfDeath === undefined) {
+            return today.getFullYear() - personObj.yearOfBirth;
+        } else {
+            return personObj.yearOfDeath - personObj.yearOfBirth;
+        }
+    }
+    
+    return oldestArray;    
 };
 
 // Do not edit below this line
