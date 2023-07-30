ae546db7dca60697a3ce845c371461bd56ad7446
Kevin Martinez <mkevinez@gmail.com>
2023-07-30 14:37:16 -0700

    completed exercise 09 and passed tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.toUpperCase().replace(/[^A-Z0-9]/g,"");
+    let reverseWord = word.split("").reverse().join("");
+    if (word === reverseWord) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
